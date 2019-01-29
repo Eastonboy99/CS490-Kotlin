deaae044d0a3ee9f2e817d42e1fdd52eaada745e
Tyler Gwin <tyler@gwins.net>
2019-01-28 19:07:09 -0500

    Working on HW#1

--- src/Homworks/HW1/hw1.kt
+++ src/Homworks/HW1/hw1.kt
@@ -2,29 +2,24 @@ package Homworks.HW1
 
 fun main() {
     for (i in 1..100) {
-        print(i.toString() + " ")
+        var printed = false
         if (i % 2 == 0) {
             print("snap ")
+            printed = true
         }
         if (i % 3 == 0) {
             print("crackle ")
+            printed = true
+
         }
         if (i % 5 == 0) {
             print("pop ")
+            printed = true
+
+        }
+        if (!printed) {
+            print(i)
         }
         println("")
     }
 }
-
-
-
-//        when (i % 2) {
-//            0 -> println("snap")
-//            1 -> println("crackle")
-//            3 -> println("pop")
-//            0, 1 -> println("snap crackle")
-//            1, 3 -> println("crackle pop")
-//            0, 3 -> println("snap pop")
-//        }
-//    }
-//}
